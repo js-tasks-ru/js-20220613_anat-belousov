6e897bbf1ac355648a1fe3186ef8875f0dfcf933
Anatoliy Belousov <anat.belousov@gmail.com>
2022-07-06 11:49:46 +0300

    решены задачи 6 модуля

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,6 +1,57 @@
 class Tooltip {
-  initialize () {
+  static isComponentExist;
+  constructor() {
+    if (Tooltip.isComponentExist) {
+      return Tooltip.isComponentExist;
+    } else {
+      Tooltip.isComponentExist = this;
+    }
+    this.initialize();
+  }
 
+  initialize () {
+    document.addEventListener('pointerover', this.pointeroverHandler);
+  }
+
+  render() {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    document.body.append(this.element);
+  }
+
+  pointeroverHandler = event => {
+    this.target = event.target.closest('[data-tooltip]');
+    if (this.target) {
+      this.target.addEventListener('pointermove', this.pointermoveHandler);
+      this.target.addEventListener('pointerout', this.pointeroutHandler);
+      this.render();
+      this.element.textContent = this.target.dataset.tooltip;
+    }
+  }
+
+  pointermoveHandler = () => {
+    const shift = 10;
+    this.element.style.left = event.clientX + shift + 'px';
+    this.element.style.top = event.clientY + shift + 'px';
+  }
+
+  pointeroutHandler = () => {
+    this.target.removeEventListener('pointermove', this.pointermoveHandler);
+    this.target.removeEventListener('pointerout', this.pointeroutHandler);
+    this.target = null;
+    this.element.remove();
+  }
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    if (this.target) {
+      this.target.removeEventListener('pointerover', this.pointeroutHandler);
+      this.target.removeEventListener('pointermove', this.pointermoveHandler);
+      this.target.removeEventListener('pointerout', this.pointeroutHandler);
+    }
+    this.remove();
   }
 }
 
